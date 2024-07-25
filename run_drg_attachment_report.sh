53502b813c07aef1f5ceea77f482e5d08d23d4b9
integration01 Github <orasenatdpltintegration01_us@oracle.com>
2024-07-25 14:23:00 +0000

    Script change

--- run_drg_attachment_report.sh
+++ run_drg_attachment_report.sh
@@ -3,7 +3,7 @@
 RG=$1
 TIMESTAMP=`date +"%m-%d-%Y_%H%M"`
 LOGFILE=drg_attachement_${RG}_${TIMESTAMP}.log
-LOGDIR=/home/opc/log/drg_attachement_execution
+LOGDIR=/home/opc/log/drg_attachment_execution
 MD_DIR=/home/opc/cloud-engineers/resources
 #TOPIC_OCID="ocid1.onstopic.oc1.iad.aaaaaaaa6yr3liaddnj7fktygwovej5ldj7tjpseav2rf7zaa4kicpophnaq"
 
@@ -11,9 +11,10 @@ source $HOME/.bash_profile
 
 echo "Started DRG script ${RG} on `date +"%m-%d-%Y %H:%M"` "
 
-python /home/opc/tenancy-admin/oci-drg-find-cidr.py -m ${MD_DIR} -ip -ipr $@ >$LOGDIR/$LOGFILE 2>&1
+python /home/opc/tenancy-admin/oci-drg-find-cidr.py -m ${MD_DIR} -ip $@ >$LOGDIR/$LOGFILE 2>&1
 
-echo "Git"
+cd ${MD_DIR}
+echo "Git from ${PWD}"
 git -C ${MD_DIR} add drg_attachments_latest.md
 git -C ${MD_DIR} commit -m "daily commit ${TIMESTAMP}"
 git -C ${MD_DIR} push
