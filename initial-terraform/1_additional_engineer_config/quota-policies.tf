c667a5830e0a48467cd7321c391ec5c29b15583f
andrew_gregory <andrew.gregory@oracle.com>
2025-01-21 13:44:43 -0500

    quotas for DBCS

--- initial-terraform/1_additional_engineer_config/quota-policies.tf
+++ initial-terraform/1_additional_engineer_config/quota-policies.tf
@@ -14,7 +14,10 @@ locals {
             for comp in local.comp_names: "set database quota /*-ecpu-count/ to 4 in compartment ${data.oci_identity_compartment.cloud-eng-comp.name}:${comp}"
         ],
         [
-            for comp in local.comp_names: "set database quota vm-block-storage-gb to 1024 in compartment ${data.oci_identity_compartment.cloud-eng-comp.name}:${comp}"
+            for comp in local.comp_names: "set database quota vm-block-storage-gb to 1500 in compartment ${data.oci_identity_compartment.cloud-eng-comp.name}:${comp}"
+        ],
+        [
+            for comp in local.comp_names: "set database quota vm-standard-e5-ocpu-count to 2 in compartment ${data.oci_identity_compartment.cloud-eng-comp.name}:${comp}"
         ]
     )
     compute_quota_statements = concat(
