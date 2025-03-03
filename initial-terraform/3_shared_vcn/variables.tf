892dc6208d8179183923ff7101ab5ffe8aac5af5
andrew_gregory <andrew.gregory@oracle.com>
2025-03-03 15:11:51 -0500

    var

--- initial-terraform/3_shared_vcn/variables.tf
+++ initial-terraform/3_shared_vcn/variables.tf
@@ -12,15 +12,20 @@ variable "private_key_password" { default = "" }
 # Required Inputs
 variable "compartment_ocid" {}
 variable "vcn_name" {}
-variable "vcn_cidr_block" {}
+variable "vcn_cidr_block" {
+    default = "172.17.99.0/24"
+}
 variable "subnet_names" {
   type = list(string)
+  default = ["web","app"]
 }
 variable "subnet_types" {
   type = list(string)
+  default = ["public","private"]
 }
 variable "subnet_cidr_blocks" {
   type = list(string)
+  default = ["172.17.99.0/25","172.17.99.128/25"]
 }
 variable "is_attach_drg" {}
 variable "drg_ocid" {
